9e1ee189acc4397e39af0c9a990e9a4b6f10da39
MarcusLau <marcuslau0903@gmail.com>
2021-03-21 11:11:49 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log ("hello")
+
+let counter = 60
+const timeStart = document.getElementById("seconds")
+const introSection = document.getElementById("IntroSection")
+
+const startButton = document.getElementById("startBtn")
+startBtn.addEventListener("click",function(){
+   
+    // timeStart.innerHTML = 60
+    // const timeTicking = setInterval(() => {
+    // counter--
+    // timeStart.innerHTML = counter
+    // }, 1000);
+
+introSection.style.display ="none"
+} )
+
